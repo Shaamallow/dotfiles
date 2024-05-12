3f1ad64ae3c86a19fc82d50e69451b020d876d43
Shaamallow <eyal.benaroche@gmail.com>
2024-05-12 18:45:13 +0200

    add obsidian
    obsidian.nvim >> neorg

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,6 +5,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.opt.conceallevel = 1
+
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
