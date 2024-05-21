b6307e775ff79656b19608c71f3194bdacc7fb3b
Shaamallow <eyal.benaroche@gmail.com>
2024-05-21 21:58:29 +0200

    Update neovim to 0.10
    - Fix audio icons on waybar
    - Fix ref in kitty conf
    - Add oil nvim plugin
    - Add better colorizer plugin

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,6 @@
 return {
-	"norcalli/nvim-colorizer.lua",
-	config = function()
-		require("colorizer").setup()
-	end,
+  "brenoprata10/nvim-highlight-colors",
+  config = function()
+    require("nvim-highlight-colors").setup()
+  end,
 }
